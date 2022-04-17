963d3ffebeec0fb63457b67f36b69107e7487bf3
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-04-17 13:36:26 +0600

    Banner section implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Home from '../src/Pages/Home/Home'
+import Home from './Pages/Home/Home/Home'
 import Navbar from './Pages/Shared/Navbar/Navbar';
 import Footer from './Pages/Shared/Footer/Footer';
 import PageNotFound from './Pages/PageNotFound/PageNotFound';
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import banner2 from '../../../images/banner/banner2.jpg'
+import './Banner.css'
+
+const Banner = () => {
+
+    return (
+        <div id='banner'>
+            <h1 id='banner-title' className='text-3xl text-center text-red-400 font font-semibold p-5'>Hijama is a Sunnah & Safe Scientific Medical Procedure</h1>
+        </div>
+    );
+};
+
+export default Banner;
