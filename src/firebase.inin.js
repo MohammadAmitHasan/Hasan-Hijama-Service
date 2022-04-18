6f7436449d1df7f7177f534bad7d6a1f69a9c096
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-04-18 18:10:22 +0600

    Environment Varibale used

--- src/firebase.inin.js
+++ src/firebase.inin.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAyXfdU8VJeUt6006x3-YqHRWkzGvg77rs",
-    authDomain: "hasan-hijama-services.firebaseapp.com",
-    projectId: "hasan-hijama-services",
-    storageBucket: "hasan-hijama-services.appspot.com",
-    messagingSenderId: "589463759246",
-    appId: "1:589463759246:web:5a927095faaf5ee1485cdf"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
